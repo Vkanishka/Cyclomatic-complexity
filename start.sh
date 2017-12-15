90870e9e793470e61f6de550d0cf458d4321ceb4
Vkanishka <kanirules5@gmail.com>
2017-12-15 19:52:35 +0000

    used Dask to find the complexity

---
+++ start.sh
@@ -0,0 +1,10 @@
+pip install flask
+pip install radon
+pip install giptython
+pip install dask distributed
+dask-scheduler
+dask-worker 127.0.0.1:8786
+dask-worker 127.0.0.1:8786
+dask-worker 127.0.0.1:8786
+python CC.py
+
