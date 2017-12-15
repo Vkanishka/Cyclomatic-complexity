827f10963973265bbbca10a37b04dbf25693ed64
Vkanishka <kanirules5@gmail.com>
2017-12-15 19:47:30 +0000

    used Dask to find the complexity

--- manual master.py
+++ manual master.py
@@ -31,6 +31,34 @@ def lodge_slave():
     return "lodged"
 
 
+# c
+@app.route('/complexity_from_slave', methods=['POST'])   # to get the complexity from slave
+def complexity_from_slave():
+    f_path = request.args.get('f_path')  # requesting a file path
+    code_complexity_req = request.json
+    f_path= code_complexity_req['f_path']
+    code_complexity= code_complexity_req['complexity']
+    print code_complexity
+    server_manager_node.f_complexity_list.append({'f_path': f_path, 'complexity': code_complexity})
+    if len(server_manager_node.f_complexity_list) == server_manager_node.total_file:
+        time_end = time.time() - server_manager_node.start_time
+        print("Ext Time taken: ", time_end)
+        print("No. of files analyzed:"+ str(len(server_manager_node.f_complexity_list)))  # shows no. of files analysed
+        print server_manager_node.f_complexity_list
+        average_complexity = 0
+        for x in server_manager_node.f_complexity_list:
+            if x['complexity'] > 0:
+                average_complexity += x['complexity']
+            else:
+                print("No files to be analyzed")
+        average_complexity = average_complexity / len(server_manager_node.f_complexity_list)
+        print("Cyclomatic Complexity of the Repository: ", average_complexity)  # shows Cyclometric complexity of the rep.
+
+                                                    # Remove the git cloned folder after use
+        if os.path.isdir(server_manager_node.path):
+            shutil.rmtree(server_manager_node.path)
+
+    return jsonify({'success': True})
 
 # b
 @app.route('/complexity_from_slave', methods=['GET'])
